d0637d38128f8bd5df729c37923dc5eed5eed526
Tyrel Huseby <thuseby.business@gmail.com>
2019-03-25 11:30:47 -0600

    added portfolio-container.js and linked it to app.js

--- src/components/app.js
+++ src/components/app.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
 import moment from 'moment';
+import PortfolioContainer from './portfolio-container';
 
 export default class App extends Component {
   render() {
     return (
       <div className='app'>
-        <h1>DevCamp React Starter</h1>
+        <h1>Tyrel Huseby</h1>
+        <PortfolioContainer />
+        <PortfolioContainer />
+        <PortfolioContainer />
         {moment().format('MMMM Do YYYY, h:mm:ss a')}
       </div>
     );
