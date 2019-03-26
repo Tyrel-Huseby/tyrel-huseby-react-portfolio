14c1d21e8e1f6cd68471ce2809ee4114c5f77394
Tyrel Huseby <thuseby.business@gmail.com>
2019-03-26 09:43:55 -0600

    added url values

--- src/components/app.js
+++ src/components/app.js
@@ -2,7 +2,6 @@ import React, { Component } from "react";
 import moment from "moment";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
-import PortfolioContainer from "./portfolio/portfolio-container";
 import NavigationContainer from "./navigation/navigation-container";
 import Home from "./pages/home";
 import About from "./pages/about";
@@ -26,9 +25,8 @@ export default class App extends Component {
           </div>
         </Router>
 
-        <h1>Jordan Hudgens Portfolio</h1>
+        <h1>Tyrel Huseby Portfolio</h1>
         <div>{moment().format("MMMM Do YYYY, h:mm:ss a")}</div>
-        <PortfolioContainer />
       </div>
     );
   }
--- src/components/portfolio/portfolio-container.js
+++ src/components/portfolio/portfolio-container.js
@@ -10,12 +10,23 @@ export default class PortfolioContainer extends Component {
       pageTitle: "Welcome to my portfolio",
       isLoading: false,
       data: [
-        { title: "Quip", category: "eCommerce" },
-        { title: "Eventbrite", category: "Scheduling"  },
-        { title: "Ministry Safe", category: "Enterprise"  },
-        { title: "SwingAway", category: "eCommerce"  }
+        { title: "Quip",
+          category: "eCommerce", 
+          slug: "quip" },
+        { title: "Eventbrite", 
+          category: "Scheduling",
+          slug: "eventbrite" },
+        {
+          title: "Ministry Safe",
+          category: "Enterprise",
+          slug: "ministry-safe"
+        },
+        { title: "SwingAway", 
+          category: "eCommerce", 
+          slug: "swingaway" }
       ]
     };
+
     this.handleFilter = this.handleFilter.bind(this);
   }
 
@@ -24,27 +35,35 @@ export default class PortfolioContainer extends Component {
       data: this.state.data.filter(item => {
         return item.category === filter;
       })
-    })
-  }
-  portfolioItems() {
-    return this.state.data.map(item => {
-      return <PortfolioItem title={item.title} url={"google.com"} />;
     });
   }
 
-  
+  portfolioItems() {
+    return this.state.data.map(item => {
+      return (
+        <PortfolioItem title={item.title} url={"google.com"} slug={item.slug} />
+      );
+    });
+  }
 
   render() {
     if (this.state.isLoading) {
       return <div>Loading...</div>;
     }
+
     return (
       <div>
         <h2>{this.state.pageTitle}</h2>
 
-        <button onClick = {() => this.handleFilter('eCommerce')}>eCommerce</button>
-        <button onClick = {() => this.handleFilter('Enterprise')}>Enterprise</button>
-        <button onClick = {() => this.handleFilter('Scheduling')}>Scheduling</button>
+        <button onClick={() => this.handleFilter("eCommerce")}>
+          eCommerce
+        </button>
+        <button onClick={() => this.handleFilter("Scheduling")}>
+          Scheduling
+        </button>
+        <button onClick={() => this.handleFilter("Enterprise")}>
+          Enterprise
+        </button>
 
         {this.portfolioItems()}
       </div>
