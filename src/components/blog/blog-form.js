3c9c4dcde2e3504d267ead3dee46eed19426887e
Tyrel Huseby <thuseby.business@gmail.com>
2019-04-01 12:12:51 -0600

    Fixed bugs and errors

--- src/components/blog/blog-form.js
+++ src/components/blog/blog-form.js
@@ -5,7 +5,7 @@ import DropzoneComponent from "react-dropzone-component";
 import filepickerCss from "../../../node_modules/react-dropzone-component/styles/filepicker.css";
 import dropzoneCss from "../../../node_modules/dropzone/dist/min/dropzone.min.css";
 
-import RichTextEditor from "..forms/rich-text-editor";
+import RichTextEditor from "../forms/rich-text-editor";
 export default class BlogForm extends Component {
   constructor(props) {
     super(props);
--- src/components/pages/contact.js
+++ src/components/pages/contact.js
@@ -28,7 +28,7 @@ export default function() {
               <FontAwesomeIcon icon="envelope" />
             </div>
 
-            <div className="text">jordan@example.com</div>
+            <div className="text">thuseby.business@gmail.com</div>
           </div>
 
           <div className="bullet-point-group">
@@ -36,7 +36,7 @@ export default function() {
               <FontAwesomeIcon icon="map-marked-alt" />
             </div>
 
-            <div className="text">Lehi, UT</div>
+            <div className="text">Fallon, MT</div>
           </div>
         </div>
       </div>
